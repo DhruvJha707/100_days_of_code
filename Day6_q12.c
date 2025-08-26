ed493b4afc6abedbec6cdc4ed8e8b862ccbcd97c
DhruvJha707 <dhruvjha707@gmail.com>
2025-08-26 10:52:16 +0530

    Initial commit

---
+++ Day6_q12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n ;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    if (n>= 0) {
+        if (n== 0) {
+            printf("The number is Zero.\n");
+        } else {
+            printf("The number is Positive.\n");
+        }
+    } else {
+        printf("The number is Negative.\n");
+    }
+
+    return 0;
+}
+    
