40553345fc6df77cb525145c9a778968fd572af7
DhruvJha707 <dhruvjha707@gmail.com>
2025-08-26 10:55:28 +0530

    Initial commit

--- Day6_q11.c
+++ Day6_q11.c
@@ -1,8 +1,14 @@
+// Write a program to input an integer and check whether it is even or odd using if–else.
+
+
+
 #include<stdio.h>
 int main(){
-int n ;
+int n ;    // user input 
 printf("Enter the number : ");
 scanf("%d",&n);
+
+// using if else to check even or odd
 
 if(n%2==0){
     printf("%d is a even number ",n);
--- Day6_q12.c
+++ Day6_q12.c
@@ -1,8 +1,13 @@
+//Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
 #include<stdio.h>
 int main(){
-    int n ;
+    int n ;    // taking input from user 
     printf("Enter a number : ");
     scanf("%d",&n);
+
+    // using nested if else 
+
     if (n>= 0) {
         if (n== 0) {
             printf("The number is Zero.\n");
