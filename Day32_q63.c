c04b64af6685be37f94648b511f460abc40084a3
DhruvJha707 <dhruvjha707@gmail.com>
2025-09-30 16:49:24 +0530

    Initial commit

---
+++ Day32_q63.c
@@ -0,0 +1,34 @@
+// Q63: Merge two arrays.
+
+#include<stdio.h>
+int main(){
+    int arr[n];
+    printf("Enter the no. of elements to be stored  in 1 : ");
+    scanf("%d",&n);
+
+
+    int arr[m];
+    printf("Enter the no. of elements to be stored in 2 : ");
+    scanf("%d",&m);
+
+    int sum = 0;
+
+    sum = arr[n] + arr[m];
+
+    printf("%d",sum);
+
+
+    return 0;
+}
+
+/*
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+
+*/
